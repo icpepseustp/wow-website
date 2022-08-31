e6e022e19637af1fef4e1373f8d97744d59da45a
Kurt Timajo <djkurttimajo@gmail.com>
2022-08-31 13:10:08 +0800

    feat: final registration component

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import QR from "../images/Registration/ICpEP_SE_Registration.png";
+import QR from "../images/Registration/ICpEers-registration-qr.png";
 
 export const Registration = () => {
   return (
@@ -7,7 +7,7 @@ export const Registration = () => {
       <h1 className="font-semibold text-2xl lg:text-4xl">
         Become an ICpEP.SE Volunteer
       </h1>
-      <p className="font-base m-6">Scan the QR Code below</p>
+      <p className="font-base text-xl m-6">Scan the QR Code below</p>
 
       <div className="">
         <img
@@ -17,12 +17,12 @@ export const Registration = () => {
         />
       </div>
 
-      <p className="m-6">Or register here: </p>
+      <p className="m-6 text-xl">Or register here: </p>
       <button
         type="button"
         class="text-white uppercase bg-[#0078C2] hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-semibold rounded-[50px] text-base px-10 py-1.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
       >
-        <a href="https://icpepse-ustp.org" target="_blank">
+        <a href="https://qrco.de/bdHeq7" target="_blank">
           Register
         </a>
       </button>
